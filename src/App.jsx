8e8a076eca1c324fe9dba19e006e946f4d297f57
Bais26 <baisyufan2004@gmail.com>
2024-11-17 01:24:16 +0700

    [BAIS][MODIFY]-modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blogs from './pages/Blogs';
-import Service from './pages/Service';
-import Contact from './pages/Contact';
-import NewsList from './component/NewsList';
-import ErrorPage from './pages/ErrorPage';
-import Index from './router/Route';
+
+// import ErrorPage from './pages/ErrorPage';
+import Index from './route';
+// import About from './pages/About';
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Index />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blogs" element={<Blogs />}>
-            <Route path=":source/:category" element={<NewsList />} />
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
+          <Route index element={< Home/>} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="*" element={<ErrorPage />} /> */}
         </Route>
       </Routes>
     </Router>
