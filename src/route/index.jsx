27a5e6a69e0aca4ccb48744d3573593df3387948
Bais26 <baisyufan2004@gmail.com>
2024-11-17 01:25:24 +0700

    [BAIS][MODIFY]-modify route

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { Outlet } from 'react-router-dom'
 
 function index() {
   return (
     <div>
-      route
+        {/* <Navbar/> */}
+      <main>
+        <Outlet />
+      </main>
+        <Footer/>
     </div>
   )
 }
