c2915ad968246da52d9db535778e6f79036d75c3
Bais26 <baisyufan2004@gmail.com>
2024-11-18 21:30:16 +0700

    [BAIS][MODIFY]- update route

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+// import NavbarHero from '../components/NavbarHero'
+import Navbar from '../components/Navbar' 
 import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 
 function index() {
   return (
     <div>
-        {/* <Navbar/> */}
+         {/* <NavbarHero /> */}
+         <Navbar/>
       <main>
         <Outlet />
       </main>
