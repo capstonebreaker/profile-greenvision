f908e42757c90d57d516cec22bfbf754a09581f9
Bais26 <baisyufan2004@gmail.com>
2024-11-22 03:14:30 +0700

    [BAIS][MODIFY]-modify Home site

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './Hero'
 import About from './About'
+import Feature from './Feature'
+import Team from './Team'
 
 function Home() {
   return (
     <div>
       <Hero/>
       <About/>
+      <Feature/>
+      <Team/>
     </div>
   )
 }
