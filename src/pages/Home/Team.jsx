e783112e913a2eaa055e149203592ea9213d0603
Bais26 <baisyufan2004@gmail.com>
2024-11-22 03:17:12 +0700

    [BAIS][CREATE]-slicing Teams apps figma

---
+++ src/pages/Home/Team.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Team() {
+    return (
+        <div class="bg-[url('/assets/Background1.png')] bg-cover bg-center h-screen">
+
+        </div>
+    )
+}
+
+export default Team
